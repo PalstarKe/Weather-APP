e8d88cfce74d649e3b225e0661d5c1073d359a95
Palstar_Ke <maishwriter2@gmail.com>
2024-01-18 22:32:45 +0300

    Initial Commit

---
+++ Weather APP/Weather_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_APPApp.swift
+//  Weather APP
+//
+//  Created by kejos on 18/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Weather_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
