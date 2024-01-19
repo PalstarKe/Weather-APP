f5c5a657cd45b00bcd134cd80c8e6fea89c6f5f8
Palstar_Ke <maishwriter2@gmail.com>
2024-01-19 03:38:13 +0300

    Edited the icon and City name

--- Weather APP/ContentView.swift
+++ Weather APP/ContentView.swift
@@ -2,20 +2,30 @@
 //  ContentView.swift
 //  Weather APP
 //
-//  Created by kejos on 18/01/2024.
+//  Created by Codevibe on 18/01/2024.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea(.all)
+            VStack{
+                Text("Nairobi Kenya")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundStyle(.white)
+                    .padding()
+                VStack{
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .frame(width: 180, height: 180)
+                }
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
